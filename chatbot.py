85953e923f52a86b1d0b55307e99ef776b76f341
omarahmed-cmd <60565615+omarahmed-cmd@users.noreply.github.com>
2023-03-03 11:29:29 +0200

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+api_key = "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"
+import openai
+import os
+os.environ['openai_key'] = api_key
+openai.api_key = os.environ['openai_key']
+keep_prompting = True
+while keep_prompting:
+    prompt = input("What is your question? Type exit if done!!")
+    if prompt.lower() == 'exit':
+        keep_prompting = False
+    else:
+        response = openai.Completion.create(engine='text-davinci-003', prompt= prompt,max_tokens=4000)
+# for code model try to replace 'text-davinci-003' by 'code-davinci-002' to just output only code
+
+    print(response['choices'][0]['text'])
