057a67a72ec66c25d9b30fc03dd3466b8a4b2349
New <60565615+omarahmed-cmd@users.noreply.github.com>
2023-05-11 18:37:18 +0300

    new chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,13 @@
-api_key = "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"
-import openai
-import os
-os.environ['openai_key'] = api_key
-openai.api_key = os.environ['openai_key']
-"""
+from llama_cpp import Llama
+
+llm = Llama(model_path='models/ggml-vicuna-13b-4bit-rev1.bin')
 keep_prompting = True
 while keep_prompting:
-    prompt = input("What is your question? Type exit if done!!\n")
+    prompt = input("What is your question? Type exit if done!! \n")
     if prompt.lower() == 'exit':
         keep_prompting = False
+        break
     else:
-        response = openai.Completion.create(engine='text-davinci-003', prompt= prompt,max_tokens=4000)
-# for code model try to replace 'text-davinci-003' by 'code-davinci-002' to just output only code
+        output = llm(prompt)
+        print(output['choices'][0]['text'])
 
-    print(response['choices'][0]['text'])
-"""
-
-def get_answer(question):
-    response = openai.Completion.create(engine='text-davinci-003', prompt= question,max_tokens=4000)
-    return response['choices'][0]['text']
