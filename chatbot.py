b7d2524cb57631500ebd802d26050418d955da32
omarahmed-cmd <60565615+omarahmed-cmd@users.noreply.github.com>
2023-07-10 22:56:12 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,28 +1,3 @@
-
-# api_key = "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"
-# import openai
-# import os
-# os.environ['openai_key'] = api_key
-# openai.api_key = os.environ['openai_key']
-# # """
-# # keep_prompting = True
-# # while keep_prompting:
-# #     prompt = input("What is your question? Type exit if done!!\n")
-# #     if prompt.lower() == 'exit':
-# #         keep_prompting = False
-# #     else:
-# #         response = openai.Completion.create(engine='text-davinci-003', prompt= prompt,max_tokens=4000)
-# # # for code model try to replace 'text-davinci-003' by 'code-davinci-002' to just output only code
-
-# #     print(response['choices'][0]['text'])
-# # """
-
-# def get_answer(question):
-#     response = openai.Completion.create(engine='text-davinci-003', prompt= question,max_tokens=4000)
-#     return response['choices'][0]['text']
-
-# get_answer("What is the best programming language?")
-
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 # Load the model and tokenizer
