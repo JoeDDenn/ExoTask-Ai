6adac49f63d761846cb563ea7ba3c59877ff2f21
omarahmed-cmd <60565615+omarahmed-cmd@users.noreply.github.com>
2023-07-10 22:59:12 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,23 +4,15 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("declare-lab/flan-alpaca-large")
 model = AutoModelForSeq2SeqLM.from_pretrained("declare-lab/flan-alpaca-large")
 
-# Tokenize your prompt text
-input_text = "what is AI?"
-inputs = tokenizer.encode(input_text, return_tensors="pt")
-
-# Generate a response using the model
-outputs = model.generate(inputs, max_length=128)
-
-# Decode and print the generated output
-output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-# print(output_text)
-# chatbot.py
 
 def get_answer(input_text):
+    # Tokenize your prompt text
     inputs = tokenizer.encode(input_text, return_tensors="pt")
-    
+
+    # Generate a response using the model
     outputs = model.generate(inputs, max_length=128)
     
+    # Decode and print the generated output
     output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     return output_text.strip()
